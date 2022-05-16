beecc9493c284756c1a27a6a39e8ecb692072a63
sebbycorp <sebastian@maniak.io>
2022-05-16 11:21:55 +0000

    hhh

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "template_file" "init" {
   vars = {
     HOSTNAME         = "bigip-nonprod.maniak.academy"
     DNS_ADDRESS      = "8.8.8.8"
-    NTP_ADDRESS      = "8.8.8.8"
+    NTP_ADDRESS      = "1.1.1.1"
     GUEST_PASSWORD   = "W3lcome098!"
     EXTERNAL_ADDRESS = "10.20.1.100/24"
     EXTERNAL_VLAN_ID = "1"

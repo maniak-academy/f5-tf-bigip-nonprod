dd15b811112c3197b8d245a186d9b9c408d9c07b
sebbycorp <sebastian@maniak.io>
2022-05-16 13:18:20 +0000

    updates

---
+++ infrastructure/variables.tf
@@ -0,0 +1,40 @@
+variable "HOSTNAME" {
+  description = "HOSTNAME"
+  default     = ""
+}
+variable "DNS_ADDRESS" {
+  description = "DNS_ADDRESS"
+  default     = ""
+}
+variable "NTP_ADDRESS" {
+  description = "NTP_ADDRESS"
+  default     = ""
+}
+variable "GUEST_PASSWORD" {
+  description = "GUEST_PASSWORD"
+  default     = ""
+}
+variable "EXTERNAL_ADDRESS" {
+  description = "EXTERNAL_ADDRESS"
+  default     = ""
+}
+variable "EXTERNAL_VLAN_ID" {
+  description = "EXTERNAL_VLAN_ID"
+  default     = ""
+}
+variable "INTERNAL_ADDRESS" {
+  description = "INTERNAL_ADDRESS"
+  default     = ""
+}
+variable "INTERNAL_VLAN_ID" {
+  description = "INTERNAL_VLAN_ID"
+  default     = ""
+}
+variable "DEFAULT_ROUTE" {
+  description = "DEFAULT_ROUTE"
+  default     = ""
+}
+variable "ALLOWED_IP" {
+  description = "DEFAULT_ROUTE"
+  default     = ""
+}

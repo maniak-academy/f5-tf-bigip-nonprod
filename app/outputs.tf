1326432ebc1195706e48b6e16bc5440179f00cf5
sebbycorp <sebastian@maniak.io>
2022-05-20 17:20:21 +0000

    tt

--- app/outputs.tf
+++ app/outputs.tf
@@ -1,9 +1,9 @@
-output "cert" {
-  value = vault_pki_secret_backend_cert.app.certificate
-}
-output "key" {
-  value = vault_pki_secret_backend_cert.app.private_key
-}
-output "chain" {
-  value = vault_pki_secret_backend_cert.app.ca_chain
-}
+# output "cert" {
+#   value = vault_pki_secret_backend_cert.app.certificate
+# }
+# output "key" {
+#   value = vault_pki_secret_backend_cert.app.private_key
+# }
+# output "chain" {
+#   value = vault_pki_secret_backend_cert.app.ca_chain
+# }

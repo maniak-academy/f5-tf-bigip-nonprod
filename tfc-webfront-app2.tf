c6127b54c73b8bfdade50be9938b323fe6c4fc65
sebbycorp <sebastian.maniak@hashicorp.com>
2022-09-07 17:22:58 -0400

    updates

--- tfc-webfront-app2.tf
+++ tfc-webfront-app2.tf
@@ -1,11 +1,11 @@
 module "tfc-webfront-app2" {
-  source       = "./applications"
-  tenant       = "tfc-webfront-app2"
-  common_name  = "webfrontapp2.example.com"
-  as3tmpl      = "https"
-  vip_address  = "10.99.99.13"
-  pki_name     = "example-dot-com"
+  source            = "./applications"
+  tenant            = "tfc-webfront-app2"
+  common_name       = "webfrontapp2.example.com"
+  as3tmpl           = "https"
+  vip_address       = "10.99.99.13"
+  pki_name          = "example-dot-com"
   pool_members_port = "80"
-  monitor = "http"
-  pool_members = ["10.10.2.1", "10.10.2.2"]
+  monitor           = "http"
+  pool_members      = ["10.10.2.1", "10.10.2.2"]
 }

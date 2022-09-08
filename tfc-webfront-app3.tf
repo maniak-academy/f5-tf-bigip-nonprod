69ea1ef9e68128e9eb5e5dedda40d8362c3fcca2
sebbycorp <sebastian.maniak@hashicorp.com>
2022-09-07 23:03:19 -0400

    updates

---
+++ tfc-webfront-app3.tf
@@ -0,0 +1,11 @@
+module "tfc-webfront-app3" {
+  source            = "./apps-module"
+  tenant            = "tfc-webfront-app2"
+  common_name       = "webfront3.maniak.academy"
+  as3tmpl           = "fullhttps"
+  vip_address       = "10.99.99.14"
+  pki_name          = "example-dot-com"
+  pool_members_port = "443"
+  monitor           = "https"
+  pool_members      = ["10.10.5.1", "10.10.5.2"]
+}

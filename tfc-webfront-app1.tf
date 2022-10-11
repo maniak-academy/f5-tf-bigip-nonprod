e0c8fcff22e703288a5c19c82ab7ff920ba90b35
Sebastian Maniak <sebastian@maniak.io>
2022-10-11 11:55:44 -0400

    Update tfc-webfront-app1.tf

--- tfc-webfront-app1.tf
+++ tfc-webfront-app1.tf
@@ -4,7 +4,7 @@ module "tfc-webfront-app1" {
   common_name       = "webfrontapp1.example.com"
   as3tmpl           = "https"
   vip_address       = "10.99.99.11"
-  pki_name          = "example-dot-com"
+  pki_name          = "maniak-dot-academy"
   pool_members_port = "80"
   monitor           = "http"
   pool_members      = ["10.10.2.1", "10.10.2.2"]

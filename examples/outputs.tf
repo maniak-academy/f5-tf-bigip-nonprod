1b242cd12953f5f8631f5e5b9e2bc21272e5feb8
SebbyCorp <sebastian@maniak.io>
2023-02-04 15:52:16 -0500

    updates

--- examples/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "cert" {
-#   value = vault_pki_secret_backend_cert.app.certificate
-# }
-# output "key" {
-#   value = vault_pki_secret_backend_cert.app.private_key
-# }
-# output "chain" {
-#   value = vault_pki_secret_backend_cert.app.ca_chain
-# }
